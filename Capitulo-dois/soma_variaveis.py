24d6b0bf11447b73b920bef4e6685d527399ec3b
Raissa Cavalcanti <154569377+cavalcantiraissas@users.noreply.github.com>
2026-01-01 13:27:11 -0300

    Exercicio 2.5 || Soma de três variáveis

---
+++ Capitulo-dois/soma_variaveis.py
@@ -0,0 +1,6 @@
+primeiro_valor = int(input("Digite o primeiro valor:\n"))
+segundo_valor = int(input("Digite o segundo valor:\n"))
+terceiro_valor = int(input("Digite o valor valor:\n"))
+
+soma = primeiro_valor + segundo_valor + terceiro_valor
+print(f"O resultado da soma dos tres valores equivale a: {soma}")
