e699febcbf35701e2823b928eca466fb38150819
Raissa Cavalcanti <154569377+cavalcantiraissas@users.noreply.github.com>
2026-01-02 15:43:08 -0300

    Exercicio 3.6 || Aluno Aprovado
    Decidir se um aluno foi ou não aprovado. Para ser aprovado, todas as médias do aluno devem ser maiores que 7. Considerando que o aluno cursa apenas três matérias, e que a nota de cada uma está armazenada nas seguintes variáveis: materia1, materia2 e materia3

---
+++ Capitulo-tres/aluno-media.py
@@ -0,0 +1,8 @@
+materia1 = float(input("Digite a sua nota na primeira matéria:\n"))
+materia2 = float(input("Digite a sua nota na segunda matéria:\n"))
+materia3 = float(input("Digite a sua nota na terceira matéria:\n"))
+
+if materia1 > 7 and materia2 > 7 and materia3 > 7:
+    print("Você foi aprovado!\n")
+else:
+    print("Você foi reprovado!\n")
