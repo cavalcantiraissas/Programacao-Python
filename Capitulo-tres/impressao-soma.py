84c6ea96f2d88958af7ad777bc5473315262ef0e
Raissa Cavalcanti <154569377+cavalcantiraissas@users.noreply.github.com>
2026-01-02 15:54:47 -0300

    Exercicio 3.7 || Soma de dois inteiros
    Programa que pede dois números inteiros e imprime a soma dos dois números na tela

---
+++ Capitulo-tres/impressao-soma.py
@@ -0,0 +1,6 @@
+primeiro_numero = int(input("Digite o primeiro numero:\n"))
+segundo_numero = int(input("Digite o segundo numero:\n"))
+
+soma = (primeiro_numero + segundo_numero)
+
+print(f"{primeiro_numero} + {segundo_numero} = {soma}")
