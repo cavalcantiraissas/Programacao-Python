15ce082a98c09a9537f3d1aeb861b27f5ff56a41
Raissa Cavalcanti <154569377+cavalcantiraissas@users.noreply.github.com>
2026-01-04 12:16:01 -0300

    Exercicio 3.13 || Conversão Celsius -> Fahrenheit
    Programa que converte uma temperatura digitada em Celsius para Fahrenheit

---
+++ Capitulo-tres/conversao-celsius-fahrenheit.py
@@ -0,0 +1,5 @@
+print("Este programa converte uma temperatura em C em F\n")
+celsius = float(input("Quantos Celsius você gostaria de converter?\n")) 
+formula_de_conversao = (((9*celsius)/5) + 32)
+
+print(f"{celsius} Celsius equivale a {formula_de_conversao} Fahrenheit\n")
