551d8f849830bf701aeac3b2773eb860bc5b7991
Raissa Cavalcanti <154569377+cavalcantiraissas@users.noreply.github.com>
2026-01-04 12:06:20 -0300

    Exercicio 3.12 || Tempo viagem
    Programa que calcula o tempo de uma viagem de carro. Recebe a distância a percorrer e a velocidade média eperada para a viagem.

---
+++ Capitulo-tres/tempo-viagem.py
@@ -0,0 +1,6 @@
+distancia = float(input("Quantos km's serão percorridos na viagem?\n"))
+velocidade = float(input("Qual a velocidade média esperada para a viagem?\n"))
+
+tempo = distancia/velocidade
+print(f"Ao percorrer {distancia} km, em uma velocidade de {velocidade}KM/H, é estimado um tempo de: {tempo} horas.")
+
