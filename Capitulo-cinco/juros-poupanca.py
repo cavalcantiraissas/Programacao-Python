80c642f3800faec281d5fe0a691cb066aa5cd750
Raissa Cavalcanti <154569377+cavalcantiraissas@users.noreply.github.com>
2026-01-07 13:54:18 -0300

    Exercicio 5.11 || Lucro por Juros na Poupança
    Programa que recebe o depósito inicial e a taxa de juros de uma poupança. Exibe os valores mês a mês para os 24 primeiros meses. Retorna o total ganho com juros no período.

---
+++ Capitulo-cinco/juros-poupanca.py
@@ -0,0 +1,12 @@
+deposito_poupanca = float(input("Quanto você gostaria de depesotiar?: "))
+taxa_juros = float(input("Qual valor da tada de juros? "))
+
+valor_inicial = deposito_poupanca
+contador = 1
+
+while contador <= 24:
+    deposito_poupanca = deposito_poupanca + (deposito_poupanca * (taxa_juros / 100))
+    print(f"Mês: {contador} | Valor Total: {deposito_poupanca:.2f}")
+    contador = contador + 1
+lucro = deposito_poupanca - valor_inicial
+print(f"Você depositou inicialmente {valor_inicial} e, após 24 meses, você resgata {deposito_poupanca:.2f}, resultando em um lucro de {lucro:.2f}")
